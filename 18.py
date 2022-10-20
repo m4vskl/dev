a3cfb30360dbc68d7571b1ed6373e922415cfb0e
m4vskl <69226043+m4vskl@users.noreply.github.com>
2022-10-20 14:48:34 +0300

    Delete 18.py

--- 18.py
+++
@@ -1,6 +0,0 @@
-yaric= int(input("Çemberin yarıçapını giriniz: "))
-pi = 3.14 
-cev = 2*pi*yaric
-
-#Çevre değerini daha sade bir görüntü verebilmek için kullanıcıya sunmadan önce integer'a çevirdik. 
-print("Çemberin çevresi: ",int(cev) )
