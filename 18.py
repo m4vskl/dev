2d8a801f30a8e25bbc4611fffeab7b51ffb29b1c
m4vskl <n0n0rd1n4ry@gmail.com>
2022-10-20 14:47:46 +0300

    Thursday Commit

--- 18.py
+++ 18.py
@@ -3,4 +3,4 @@ pi = 3.14
 cev = 2*pi*yaric
 
 #Çevre değerini daha sade bir görüntü verebilmek için kullanıcıya sunmadan önce integer'a çevirdik. 
-print("Çemberin çevresi: ",int(cev))
+print("Çemberin çevresi: ",int(cev) )
