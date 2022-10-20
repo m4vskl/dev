889c1064d34e5e7804b5f920ad482a16bb2cf42a
m4vskl <n0n0rd1n4ry@gmail.com>
2022-10-20 16:32:17 +0300

    no message

--- Homw/21.py
+++ Homw/21.py
@@ -3,15 +3,24 @@
 sinema = 49.5
 tiyatro = 29.8
 fiyat = 0
-ind = 1
-ind_durum = input("Öğrenci misiniz ? EVET/HAYIR")
+ind = 0
+ind_durum = input("Öğrenci misiniz ? EVET/HAYIR \n ")
 if ind_durum == "EVET":
     print("%50 indirimden faydalanabilirsiniz.")
     ind = 0.5
+elif ind_durum == "HAYIR":
+    ind = 1
+else:
+    quit()
+    
 
 bilet = int(input("Sinema bileti almak için 1, tiyatro bileti için 2 yazınız."))
 
 if bilet == 1:
     fiyat = sinema*ind
+    print("Ödemeniz gereken fiyat: ", fiyat)
+
+elif bilet == 2:
+    fiyat = tiyatro*ind
     print("Ödemeniz gereken fiyat: ", fiyat)
     
