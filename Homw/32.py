ee9ad1e48d5ec9b8e908e459710b114bc975dec7
m4vskl <n0n0rd1n4ry@gmail.com>
2022-10-26 12:55:35 +0300

    edit

--- Homw/32.py
+++ Homw/32.py
@@ -5,6 +5,8 @@ a1 = input("En sevdiğiniz ilk araba markası : ")
 a2 = input("En sevdiğiniz ikinci araba markası: ")
 a3 = input("En sevdiğiniz üçüncü araba markası: ")
 # kullanıcıdan aldığımız markaları liste içerisine ekledik.
-marka.append(a1,a2,a3)
+marka.append(a1)
+marka.append(a2)
+marka.append(a3)
 
 print(marka)
